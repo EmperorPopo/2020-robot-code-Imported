c1987b79780fbd69cbcb0e37cac0c4af134660fa
EmperorPopo <60585465+EmperorPopo@users.noreply.github.com>
2021-06-16 16:18:34 -0600

    SHUT THE FUCK UP
    NOBODY CARES

--- src/main/java/org/usfirst/frc/team4944/robot/commands/TurnLeft.java
+++ src/main/java/org/usfirst/frc/team4944/robot/commands/TurnLeft.java
@@ -18,16 +18,6 @@ public class TurnLeft extends Command {
 	// SUBSYSTEMS
 	DriveSystem driveSystem;
 	
-	// VARIABLES
-	double driveGoal, headingGoal, leftInit, rightInit, angleInit;
-	
-	// DRIVE PIDS
-	DrivePID leftPID;
-	DrivePID rightPID;
-	
-	// BASIC PIDS
-	BasicPID anglePID;
-
 	// NAVX
 	AHRS gyro = new AHRS(I2C.Port.kMXP);
 	double kGyroRateCorrection;
@@ -38,65 +28,48 @@ public class TurnLeft extends Command {
 	double targetAngle;
 	double angle;
 
-
-	// CONSTANTS
-	final double driveThreshold = 100;
-
-
-	public TurnLeft(double driveGoal, double headingGoal, DrivePID leftPID, DrivePID rightPID, BasicPID anglePID, Double targetAngle) {
+	public TurnLeft(Double targetAngle) {
 		this.driveSystem = new DriveSystem();
 		requires(driveSystem);
-		this.driveGoal = driveGoal;
-		this.headingGoal = headingGoal;
-		this.leftPID = leftPID;
-		this.rightPID = rightPID;
-		this.anglePID = anglePID;
-		this.leftInit = driveSystem.getLeftEncoder();
-		this.rightInit = driveSystem.getRightEncoder();
-		this.angleInit = driveSystem.getAngle();
 		//this.angle = gyro.getRoll();
     	// kGyroRateCorrection = (robotPeriod / gyroPeriod) * gyroRateCoeff;
 		// this.angle += gyro.getRate() * kGyroRateCorrection;
 		// this.angle = Math.IEEEremainder(angle, 360.0);
-		// this.targetAngle = targetAngle;
+		this.targetAngle = targetAngle;
 		init();
 	}
 
 	public void init() {
 		System.out.println("Init");
-		// leftPID.setSetPoint(leftInit + driveGoal);
-		// rightPID.setSetPoint(rightInit + driveGoal);
-		// anglePID.setSetPoint(angleInit + headingGoal);
 		gyro.calibrate();
 	}
 
 	public void execute() {
-		this.angle = driveSystem.getAngle();
+		this.angle = this.gyro.getYaw();
     	// kGyroRateCorrection = (robotPeriod / gyroPeriod) * gyroRateCoeff;
 		// this.angle += gyro.getRate() * kGyroRateCorrection;
 		// this.angle = Math.IEEEremainder(angle, 360.0);
 		driveSystem.setLeftPower(-0.25);
 		driveSystem.setRightPower(-0.25);
-		System.out.println(this.angle);
-		System.out.println(gyro.isConnected());
-		System.out.println(gyro.isCalibrating());
+		System.out.println(this.gyro.getYaw());
+		// System.out.println(gyro.isConnected());
+		// System.out.println(gyro.isCalibrating());
 		SmartDashboard.putNumber("Gyro Yaw: ", this.angle);
 	}
 
 	public boolean isFinished() {
-		// if ((this.angle == this.targetAngle)) {
-		// 	if(this.driveSystem.getDoneDriveing()){
-		// 		System.out.println("Exited");
-		// 		driveSystem.setLeftPower(0);
-		// 		driveSystem.setRightPower(0);
-		// 		return true;
-		// 	}else{
-		// 		return false;
-		// 	}
-		// }else{
-		// 	return false;	
-		// }
+		if ((this.angle <= -90 /*Constant for now will eventually change it to a variable */)) {
+			if(this.driveSystem.getDoneDriveing()){
+				System.out.println("Exited");
+				driveSystem.setLeftPower(0);
+				driveSystem.setRightPower(0);
+				return true;
+			}else{
+				return false;
+			}
+		}else{
+			return false;	
+		}
 
-		return false;
 	}
 }
